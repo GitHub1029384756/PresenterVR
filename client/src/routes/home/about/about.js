f237f1e4d51b3aca25686418427b90d958f09d69
GitHub1029384756 <56824711+GitHub1029384756@users.noreply.github.com>
2021-03-27 04:25:55 +0000

    made changes to about section: 27/03/2021

--- client/src/routes/home/about/about.js
+++ client/src/routes/home/about/about.js
@@ -25,16 +25,18 @@ function about() {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
                     </Col>
                 </Row>
-                <hr className = "w-25"></hr>
                 <Row data-aos = "fade-right" className = "border-box my-4">
                     <Col lg = {4} className = "px-0">
                         <Image src = {Pollusion} fluid></Image>
+                        <h1 className = "text-centered text-center text-white">Enviroment</h1>
                     </Col>
                     <Col lg = {4} className = "px-0">
                         <Image src = {Exspense} fluid></Image>
+                        <h1 className = "text-centered text-center text-white">Exspense</h1>
                     </Col>
                     <Col lg = {4} className = "px-0">
                         <Image src = {Collab} fluid></Image>
+                        <h1 className = "text-centered text-center text-white">Collaboration</h1>
                     </Col>
                 </Row>
                 <Row>
